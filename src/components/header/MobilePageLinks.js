bef9e6793e4c191b83b8449c651c1ae39ac47de3
Dixon <dixon.almeida8@gmail.com>
2019-11-04 02:08:42 +0530

    added buttons

--- src/components/header/MobilePageLinks.js
+++ src/components/header/MobilePageLinks.js
@@ -9,9 +9,9 @@ const MobilePages = () => {
                 <li className="d-inline p-4">Expert Devops/Kubernetes Engineer</li>
                 <br/>
                 <br/>
-                <li className="d-inline p-4"><Link to="/"><span className="text-dark" >Blog</span></Link></li>
-                <li className="d-inline p-4"><Link to="/vlog"><span className="text-dark">Vlog</span></Link></li>
-                <li className="d-inline p-4"><Link to="/about"><span className="text-dark">About</span></Link></li>
+                <li className="d-inline p-4"><Link to="/"><span class="btn btn-info" >Blog</span></Link></li>
+                <li className="d-inline p-4"><Link to="/vlog"><span class="btn btn-info">Vlog</span></Link></li>
+                <li className="d-inline p-4"><Link to="/about"><span class="btn btn-info"className="text-dark">About</span></Link></li>
         </div>
     )
 }
