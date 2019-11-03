b9c48b16bf04d10fd12cad8260374073ade8f3fb
Dixon <dixon.almeida8@gmail.com>
2019-11-04 01:18:53 +0530

    mobile spacing

--- src/components/header/MobileBio.js
+++ src/components/header/MobileBio.js
@@ -6,10 +6,10 @@ import dj from "../../images/dj.jpg"
 const MobileBio = (props) => {
 
     return (
-        <div className="mobile-bio-main">
-            <img src={dj}  class="Author-module--author__photo--36xCH" style={{ maxWidth: `75px`, maxHeight: `75px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
-            <h4 className="mr-4 mt-4">{props.author}</h4>
-        </div>
+            <header class="mobile-bio-main">
+            <img src={dj}  className="ml-4 mt-2" style={{ maxWidth: `75px`, maxHeight: `75px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
+            <h4 className="my-4 pl-4 px-4 mb-4">{props.author}</h4>
+            </header>
     )
 }
 
