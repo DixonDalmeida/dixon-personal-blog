98a0897ed624d244d0d7b0e06d3eca76e129791b
Dixon <dixon.almeida8@gmail.com>
2019-11-04 02:04:19 +0530

    formatting

--- src/components/header/MobilePageLinks.js
+++ src/components/header/MobilePageLinks.js
@@ -4,17 +4,14 @@ import { Link } from "gatsby"
 const MobilePages = () => {
     return (
         <div className="mobile-pages-main">
-            <ul >
                 <b><li className="d-inline p-4">Dixon Almeida</li></b>
                 <br/>
                 <li className="d-inline p-4">Expert Devops/Kubernetes Engineer</li>
                 <br/>
-                <li className="d-inline p-4"><Link to="/"><span className="text-dark">Blog Home</span></Link></li>
                 <br/>
-                <li className="d-inline p-4"><Link to="/vlog"><span className="text-dark">Vlog Home</span></Link></li>
-                <br/>
+                <li className="d-inline p-4"><Link to="/"><span className="text-dark" >Blog</span></Link></li>
+                <li className="d-inline p-4"><Link to="/vlog"><span className="text-dark">Vlog</span></Link></li>
                 <li className="d-inline p-4"><Link to="/about"><span className="text-dark">About</span></Link></li>
-            </ul>
         </div>
     )
 }
