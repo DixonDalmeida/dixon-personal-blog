129c40dd8d1f88cd0e48f13a2567dc2a7d4e0553
Dixon <dixon.almeida8@gmail.com>
2019-11-03 19:02:30 +0530

    Mobile Formatting

--- src/components/header/MobileBio.js
+++ src/components/header/MobileBio.js
@@ -6,10 +6,10 @@ import dj from "../../images/dj.jpg"
 const MobileBio = (props) => {
 
     return (
-        <div >
+        <div className="mobile-bio-main">
             <img src={dj}  className="ml-4 mt-2" style={{ maxWidth: `75px`, maxHeight: `75px`, borderRadius: `50%`,boxShadow: `1px 1px 3px`}} alt="author-pic" />
             <br/>
-            <h4 className="mr-4 mt-4">{props.author}</h4>
+            <h4 className="mr-1 mt-1">{props.author}</h4>
         </div>
     )
 }
