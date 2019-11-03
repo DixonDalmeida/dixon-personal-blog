9d390db644b7c4b8ad7c6073ad0f35072bd58656
Dixon <dixon.almeida8@gmail.com>
2019-11-03 14:34:49 +0530

    Added About Me info

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,7 +20,49 @@ const AboutPage = (props) => {
                     <SEO title="About" />
                     <div className="mt-3">
                         <h2 className="heading">About</h2>
-                        <p><i>Developer Diary is a Gatsby Starter blog template created with web developers in mind, but really, anyone can use it. It's totally usable right out of the box, but minimalist enough to be easily modifiable to suit your needs.</i></p>
+                        <p><i><b>Dixon Joseph Dalmeida is Lead DevOps Engineer.</b>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Dixon has production level experience in Kubernetes ( AKS, IKS, EKS).</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Dixon writes various articles and Vlogs in LinkendIn.</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span> 
+                            <p className="d-inline-block ml-3 w-75 align-top">Experience in building end to end DevOps workflow using Jenkins, Chef, Spinnaker, Docker, Kubernetes ,Helm.</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Extensive experience in analysis of the business requirements and providing end to end solution to customer from solution architecture and design to deployment and distribution strategy based on the product.</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Extensive experience in end to end delivery of project from making POCs, requirements gathering, system architecture, design and implementation . Excellent time management skills with ability to perform under pressure & meet deadlines.</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Experience in solving  production issues under pressure.</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span> 
+                            <p className="d-inline-block ml-3 w-75 align-top">Experience managing the DevOps Squad by issuing tasks, empowering the team members, Solving issues.</p>
+                            <br></br>
+                            <span className="text-success d-inline-block" title="blazing">
+                                <FaCheckCircle size={26} style={{ color: "success" }} />
+                            </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Experience in collaborating the Developer, Ops and Test team.</p>
+                            <br></br>
+</i></p>
                         <br />
                         <h4>Features</h4>
                         <div>
